fb1a305b6db6829b5258553291fff5c6e0b86866
sutakshi <149708726+sutakshi@users.noreply.github.com>
2023-11-24 15:18:15 +0530

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,56 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    
+    vector<int> v;
+    int size;
+    
+    cout<<"enter vector size : ";
+    cin>>size;
+    
+    cout<<"---Insertion---"<<endl;
+    
+    for(int i=0;i<size;i++)
+    {
+        int x;
+        
+        cout<<"enter element : ";
+        cin>>x;
+        
+        v.push_back(x);
+    }
+    
+    cout<<endl;
+    cout<<"---Before Sorting---"<<endl;
+    
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    
+    cout<<endl;
+    cout<<"---After Sorting---"<<endl;
+    
+    for(int i=0;i<v.size();i++)
+    {
+       for(int j=i+1;j<v.size();j++)
+       {
+           if(v[i]>v[j])
+           {
+               swap(v[i],v[j]);
+           }
+       }
+    }
+    
+    for(int i=0;i<v.size();i++)
+    {
+        cout<<v[i]<<" ";
+    }
+    
+
+    return 0;
+}
